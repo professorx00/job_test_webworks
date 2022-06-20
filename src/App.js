8ad006eb7855bcc598837d3d90c9a9174d5f48e9
David Ward <professorx00@gmail.com>
2022-06-20 19:21:51 -0400

    Initalization of Application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
